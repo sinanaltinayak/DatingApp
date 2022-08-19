a03d2b6dfb98ec2acbe5a75d304a909e83f5717f
sinan.altinayak <sinan.altinayak@acibadem.com>
2022-08-19 15:54:32 +0300

    login-register authentication

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
