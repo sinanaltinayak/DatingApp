9acd63fb483b269c116133dce56e7db3652a7943
sinan.altinayak <sinan.altinayak@acibadem.com>
2022-08-19 18:20:03 +0300

    login-register frontend

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { HttpClientModule } from "@angular/common/http";
+import { NavComponent } from "./nav/nav.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./register/register.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BsDropdownModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
