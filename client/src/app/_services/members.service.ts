d0e2846d46e3528c4d76e6540a5265305ab08b3b
sinan.altinayak <sinan.altinayak@acibadem.com>
2022-09-14 16:34:03 +0300

    editprofile

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { map, Observable, of } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Member } from "../_models/member";
 
@@ -9,14 +9,32 @@ import { Member } from "../_models/member";
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) {}
 
   getMembers(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.baseUrl + "users");
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + "users").pipe(
+      map((members) => {
+        this.members = members;
+        return members;
+      })
+    );
   }
 
   getMember(username: string): Observable<Member> {
+    const member = this.members.find((x) => x.username === username);
+    if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + "users/" + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + "users", member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,6 +21,9 @@ import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { ServerErrorComponent } from "./errors/server-error/server-error.component";
 import { MemberCardComponent } from "./members/member-card/member-card.component";
 import { JwtInterceptor } from "./_interceptors/jwt.interceptor";
+import { MemberEditComponent } from "./members/member-edit/member-edit.component";
+import { NgxSpinnerModule } from "ngx-spinner";
+import { LoadingInterceptor } from "./_interceptors/loading.interceptor";
 
 @NgModule({
   declarations: [
@@ -36,6 +39,7 @@ import { JwtInterceptor } from "./_interceptors/jwt.interceptor";
     NotFoundComponent,
     ServerErrorComponent,
     MemberCardComponent,
+    MemberEditComponent,
   ],
   imports: [
     BrowserModule,
@@ -45,10 +49,12 @@ import { JwtInterceptor } from "./_interceptors/jwt.interceptor";
     ReactiveFormsModule,
     BrowserAnimationsModule,
     SharedModule,
+    NgxSpinnerModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
 })
